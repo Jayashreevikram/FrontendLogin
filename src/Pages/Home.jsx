c0c7d2ded2ebcc22f74ae9b2fd6428df0c5d69db
Jayashreevikram <kvjayshree91@gmail.com>
2025-10-21 00:58:21 +0530

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:3000/login", {
+      const res = await axios.post("https://backendlogin-nv5h.onrender.com", {
         email,
         password: pass,
       });
